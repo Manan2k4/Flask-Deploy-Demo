65fd03462f0e2fa9fa51221186f2837471179e90
Manan2k4 <ghoniyamanan@gmail.com>
2025-06-05 11:03:20 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,6 +8,10 @@ CORS(app)
 def hello():
     return jsonify(message="Hello from Flask on Vercel! 🚀")
 
+@app.route("/hello")
+def hello_user():
+    return jsonify(greet="Hey Manan! This route works too! 🎯")
+
 # Vercel expects this exact function name
 def __vercel__handler(request, response):
     return app(request.environ, response.start_response)
