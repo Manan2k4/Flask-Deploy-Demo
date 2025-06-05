6b5478aab26d441da229f31cb320b1d0724ce866
Manan2k4 <ghoniyamanan@gmail.com>
2025-06-05 10:59:14 +0530

    deployment trial #1

--- api/index.py
+++ api/index.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
+@app.route("/")
+def hello():
     return jsonify(message="Hello from Flask on Vercel! 🚀")
 
-# Required Vercel entrypoint
-def handler(request, response):
+# Vercel expects this exact function name
+def __vercel__handler(request, response):
     return app(request.environ, response.start_response)
